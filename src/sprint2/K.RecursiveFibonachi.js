3469f747b2168430a7d770a2039f2fe787a8ac91
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2021-06-28 17:55:05 +0300

    start working on K task

---
+++ src/sprint2/K.RecursiveFibonachi.js
@@ -0,0 +1,21 @@
+function solution(data) {
+  const [] = data.split('\n');
+
+
+}
+
+/*var readline = require('readline');
+var io_interface = readline.createInterface({input: process.stdin});
+
+let output_numbers = [];
+io_interface.on('line', function (line) {
+  output_numbers.push(line);
+})
+
+io_interface.on('close', function () {
+  process.stdout.write(solution(output_numbers));
+})
+ */
+
+console.log(solution());
+
