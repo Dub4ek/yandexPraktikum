0cd85178bb75a1559fbba38be27cf0e9f5fe2761
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2021-07-06 13:42:15 +0300

    done HW B task

---
+++ src/sprint2/HW/B.Calculation.js
@@ -0,0 +1,55 @@
+// 52113404
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  pop() {
+    if (this.stack.length === 0) {
+      return 'error';
+    }
+
+    return this.stack.pop();
+  }
+}
+
+function solution(value) {
+  const [data] = value.toString().split('\n');
+  const operands = data.split(' ');
+  const stack = new Stack();
+
+  operands.forEach((item) => {
+    const currentItem = parseInt(item, 10);
+
+    if (!isNaN(currentItem)) {
+      stack.push(currentItem);
+    } else {
+      const secondValue = stack.pop();
+      const firstValue = stack.pop();
+
+      if (item === '+') {
+        stack.push(firstValue + secondValue);
+      } else if (item === '-') {
+        stack.push(firstValue - secondValue);
+      } else if (item === '*') {
+        stack.push(firstValue * secondValue);
+      } else if (item === '/') {
+        stack.push(Math.floor(firstValue/secondValue));
+      }
+    }
+  });
+
+  return stack.pop();
+}
+
+let cnt, res;
+process.stdin.on('data', data => {
+  res = solution(data);
+  process.stdout.write(res + '');
+  process.exit();
+});
