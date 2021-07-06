51927641600271615501159b589d50d4ddfb47dd
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2021-07-06 12:22:11 +0300

    done HW A task

--- src/sprint2/HW/A.Deck.js
+++ src/sprint2/HW/A.Deck.js
@@ -5,71 +5,98 @@ class Dequeue {
   }
 
   push_back(value) {
-    if (this._isLimitExceed()) {
-      return 'error';
+    if (!this._canProceedAnOperation()) {
+      return this._getErrorText();
     }
 
     this.queue.push(value);
   }
 
   push_front(value) {
-    if (this._isLimitExceed()) {
-      return 'error';
+    if (!this._canProceedAnOperation()) {
+      return this._getErrorText();
     }
 
     this.queue.unshift(value);
   }
 
   pop_front() {
-    if (this.queue.length) {
-      return 'error';
+    if (!this._canProceedAnOperation()) {
+      return this._getErrorText();
     }
 
     return this.queue.shift();
   }
 
   pop_back() {
-    if (this.queue.length) {
-      return 'error';
+    if (!this._canProceedAnOperation()) {
+      return this._getErrorText();
     }
 
     return this.queue.pop();
   }
 
-  _isLimitExceed() {
-    return this.queue.length < this.size;
+  _canProceedAnOperation() {
+    return this.queue.length >= this.size || !this.queue.length;
+  }
+
+  _getErrorText() {
+    return 'error';
   }
 }
 
 function solution(values) {
   const [overallCount, operations_count, ...commands] = values;
-  const dequeue = new Dequeue(operations_count);
-  let result = [];
+  const dequeue = new Dequeue(parseInt(operations_count, 10));
+  let result = '';
 
   commands.forEach((command) => {
     if (command.includes('push_back')) {
-      result += dequeue.push_back(command.split(' ')[1]);
+      const pushResult = dequeue.push_back(command.split(' ')[1]);
+
+      if (pushResult) {
+        result += pushResult + '\n';
+      }
     }
 
     if (command.includes('push_front')) {
-      result += dequeue.push_front(command.split(' ')[1]);
+      const pushResult = dequeue.push_front(command.split(' ')[1]);
+
+      if (pushResult) {
+        result += pushResult + '\n';
+      }
     }
 
     if (command.includes('pop_back')) {
-      result += dequeue.push_back() + '\n';
+      result += dequeue.pop_back() + '\n';
     }
 
     if (command.includes('pop_front')) {
-      result += dequeue.push_front() + '\n';
+      result += dequeue.pop_front() + '\n';
     }
   });
 
-  return result.join('\n');
+  return result;
 }
 
-console.log(solution(('4\n' +
-  '4\n' +
-  'push_front 861\n' +
-  'push_front -819\n' +
-  'pop_back\n' +
-  'pop_back\n')).split('\n'));
+var readline = require('readline');
+var io_interface = readline.createInterface({input: process.stdin});
+
+let output_numbers = [];
+io_interface.on('line', function (line) {
+  output_numbers.push(line);
+})
+
+io_interface.on('close', function () {
+  process.stdout.write(solution(output_numbers));
+})
+
+/*
+console.log(solution(('6\n' +
+  '6\n' +
+  'push_front -201\n' +
+  'push_back 959\n' +
+  'push_back 102\n' +
+  'push_front 20\n' +
+  'pop_front\n' +
+  'pop_back\n').split('\n')));*/
