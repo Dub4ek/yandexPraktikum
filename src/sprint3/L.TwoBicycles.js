1537a8b368d8e7adfcce6ad2ba34ebffb701f351
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2021-07-19 20:30:45 +0300

    add sprint 3 first task

--- src/sprint3/L.TwoBicycles.js
+++ src/sprint3/L.TwoBicycles.js
@@ -1,32 +1,45 @@
 function solution(data) {
   const [count, priceTrend, bicyclePrice] = data;
-  let start = 0;
-  const priceTrendCol = priceTrend.split(' ').map(item => parseInt(item, 10));
-  let end = parseInt(count, 10);
-  let pivot = Math.floor((start + end) / 2);
-  let currentValue = priceTrendCol[pivot];
-  const currentBicyclePrice = parseInt(bicyclePrice, 10);
 
-  while (currentBicyclePrice !== currentValue) {
-    if (end <= start) {
+  function findIndex(index) {
+    let start = 0;
+    const priceTrendCol = priceTrend.split(' ').map(item => parseInt(item, 10));
+    let end = parseInt(count, 10) - 1;
+    let pivot = Math.floor((start + end) / 2);
+    let currentValue = priceTrendCol[pivot];
+    const currentBicyclePrice = parseInt(bicyclePrice, 10) * index;
+    let prevPivot = -1;
+
+    while (end > start && pivot !== prevPivot) {
+      if (currentValue === currentBicyclePrice) {
+        break;
+      }
+      if (currentValue > currentBicyclePrice) {
+        end = pivot;
+      } else {
+        start = pivot + 1;
+      }
+
+      prevPivot = pivot;
+      pivot = Math.floor((start + end) / 2);
+
+      if (pivot < parseInt(count, 10)) {
+        currentValue = priceTrendCol[pivot];
+      }
+    }
+
+    if (currentValue < currentBicyclePrice) {
       return -1;
     }
 
-    if (currentValue > currentBicyclePrice) {
-      end = pivot;
-    } else {
-      start = pivot + 1;
-    }
-
-    pivot = Math.floor((start + end) / 2);
-    currentValue = priceTrendCol[pivot];
+    return pivot + 1;
   }
 
-  return currentValue;
+  return `${findIndex(1)} ${findIndex(2)}`;
 
 }
 
-/* var readline = require('readline');
+/*var readline = require('readline');
 var io_interface = readline.createInterface({input: process.stdin});
 
 let output_numbers = [];
@@ -36,9 +49,10 @@ io_interface.on('line', function (line) {
 
 io_interface.on('close', function () {
   process.stdout.write(solution(output_numbers));
-})
-*/
+})*/
+
+
 
 console.log(solution(('6\n' +
-  '1 2 4 4 6 8\n' +
-  '3').split('\n')));
+  '1 1 4 4 4 4\n' +
+  '1').split('\n')));
