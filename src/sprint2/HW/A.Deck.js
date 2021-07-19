a39fabc154853370a702ec80ec62204438c33604
Andrei Starodubtcev <dub4ek@gmail.com>
2021-07-19 11:22:25 +0300

    working on L task

--- src/sprint2/HW/A.Deck.js
+++ src/sprint2/HW/A.Deck.js
@@ -52,10 +52,10 @@ class Dequeue {
 
 class DequeueRingBuffer {
   constructor(size) {
-    this.queue = [];
+    this.queue = new Array(2 * size);
     this.size = 0;
-    this.tail = 0;
-    this.head = 0;
+    this.tail = size;
+    this.head = size;
     this.max_n = size;
   }
 
@@ -64,24 +64,40 @@ class DequeueRingBuffer {
       return this._getErrorText();
     }
 
-    const value = this.queue[this.head];
-    this.queue[this.head] = undefined;
-    this.head = (this.head + 1) % this.max_n;
+    const value = this.queue[this.head + 1];
+    this.queue[this.head + 1] = undefined;
+    this.head = (this.head + 1);
     this.size -= 1;
 
     return value;
   }
 
   pop_back() {
+    if (this._isEmptyQueue()) {
+      return this._getErrorText();
+    }
 
+    const value = this.queue[this.tail - 1];
+    this.queue[this.tail - 1] = undefined;
+    this.tail = (this.tail - 1);
+    this.size -= 1;
+
+    return value;
   }
 
-  push_front() {
+  push_front(value) {
     if (this._isLimitExceed()) {
       return this._getErrorText();
     }
 
+    this.queue[this.head] = value;
 
+    if (this.head === this.tail) {
+      this.tail += 1;
+    }
+
+    this.head = (this.head - 1);
+    this.size += 1;
   }
 
   push_back(value) {
@@ -90,16 +106,16 @@ class DequeueRingBuffer {
     }
 
     this.queue[this.tail] = value;
-    this.tail = (this.tail + 1) % this.max_n;
+    this.tail = (this.tail + 1);
     this.size += 1;
   }
 
   _isLimitExceed() {
-    return this.queue.length >= this.size;
+    return this.queue.length > this.max_n;
   }
 
   _isEmptyQueue() {
-    return this.queue.length === 0;
+    return this.size === 0;
   }
 
   _getErrorText() {
@@ -109,7 +125,7 @@ class DequeueRingBuffer {
 
 function solution(values) {
   const [overallCount, operations_count, ...commands] = values;
-  const dequeue = new Dequeue(parseInt(operations_count, 10));
+  const dequeue = new DequeueRingBuffer(parseInt(operations_count, 10));
   let result = '';
 
   commands.forEach((command) => {
@@ -140,7 +156,7 @@ function solution(values) {
 
   return result;
 }
-
+/*
 var readline = require('readline');
 var io_interface = readline.createInterface({input: process.stdin});
 
@@ -152,13 +168,15 @@ io_interface.on('line', function (line) {
 io_interface.on('close', function () {
   process.stdout.write(solution(output_numbers));
 })
-
-
-/*console.log(solution(('6\n' +
-  '6\n' +
-  'push_front -201\n' +
-  'push_back 959\n' +
-  'push_back 102\n' +
-  'push_front 20\n' +
+*/
+console.log(solution(('7\n' +
+  '7\n' +
   'pop_front\n' +
-  'pop_back\n').split('\n')));*/
+  'pop_front\n' +
+  'push_front 741\n' +
+  'push_front 648\n' +
+  'pop_front\n' +
+  'pop_back\n' +
+  'pop_front\n').split('\n')));
+
+// [0,0,20,-201,959,102]
