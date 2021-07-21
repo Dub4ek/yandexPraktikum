c6e1beaf9c4ae0ebaf3583c4a1de9417e849d169
Andrei_Starodubtcev <andrei_starodubtcev@epam.com>
2021-07-21 10:57:11 +0300

    add sprint 3 first task

---
+++ src/sprint3/B.Combinations.js
@@ -0,0 +1,30 @@
+
+function solution(value) {
+  const currentValue = parseInt(value, 10);
+  const symbols = {
+    2:'abc',
+    3:'def',
+    4:'ghi',
+    5:'jkl',
+    6:'mno',
+    7:'pqrs',
+    8:'tuv',
+    9:'wxyz'
+  };
+
+
+}
+
+/*var readline = require('readline');
+var io_interface = readline.createInterface({input: process.stdin});
+
+let output_numbers = [];
+io_interface.on('line', function (line) {
+  output_numbers.push(line);
+})
+
+io_interface.on('close', function () {
+  process.stdout.write(solution(output_numbers));
+})*/
+
+//console.log(solution(('2').split('\n')));
