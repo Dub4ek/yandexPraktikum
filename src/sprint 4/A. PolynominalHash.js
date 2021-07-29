3a2e3d905182bbad6b7cf2d4942617d16f1c5a6e
Andrei Starodubtcev <dubchek@bk.ru>
2021-07-29 19:32:26 +0300

    Sprint 4 done A task

---
+++ src/sprint 4/A. PolynominalHash.js
@@ -0,0 +1,34 @@
+function solution(data) {
+  const [a, m, letters] = data;
+  const collection = letters.split('');
+  let result = 0;
+
+  collection.forEach((item, i) => {
+    const currentItem = item.charCodeAt(0);
+
+    if (i < collection.length - 1) {
+      result = ((result + currentItem) * parseInt(a, 10)) % parseInt(m, 10);
+    } else {
+      result = (result + currentItem) % parseInt(m, 10);
+    }
+  });
+
+  return result;
+}
+
+var readline = require('readline');
+var io_interface = readline.createInterface({input: process.stdin});
+
+let output_numbers = [];
+io_interface.on('line', function (line) {
+  output_numbers.push(line);
+})
+
+io_interface.on('close', function () {
+  console.log(solution(output_numbers));
+  //process.stdout.write(solution(output_numbers));
+})
+
+/*console.log(solution(('123\n' +
+  '100003\n' +
+  'HaSH\n').split('\n')));*/
