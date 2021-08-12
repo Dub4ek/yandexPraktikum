b29a9b333960b0802dbe24ad9e2137b41f9ebb14
Andrei Starodubtcev <dubchek@bk.ru>
2021-08-12 12:46:20 +0300

    Sprint 4 done HW

--- src/sprint 4/HW/A. SearchSystem.js
+++ src/sprint 4/HW/A. SearchSystem.js
@@ -24,72 +24,75 @@ function solution(data) {
     return result;
   }
 
-  const searchList = parseString(documentsCollection);
-  const requestList = requests.map(item => item.split(' '));
-  let results = [];
-  let answer = [];
+  function comparator(a, b) {
+    const [valueA, indexA] = a;
+    const [valueB, indexB] = b;
 
-  function addFive(col, value) {
-    let changed = false;
-
-    if (col.length === 0) {
-      col.push(value);
-      return;
+    if (valueA === valueB) {
+      return indexA - indexB;
     }
 
-    if (col[0][0] < value[0]) {
-      col.unshift(value)
-    } else {
-      for (let i = 0; i < col.length; i++) {
-        if (col[i][0] <= value[0] && col[i + 1] && col[i + 1][0] < value[0]) {
-          let temp = [...col[i]];
-          let tempCol = [];
-
-          if (temp[0] === value[0]) {
-            tempCol = temp[1] < value[1] ? [temp, value] : [value, temp]
-          } else {
-            tempCol = temp[0] > value[0] ? [temp, value] : [value, temp];
-          }
-
-          col.splice(i, 1, ...tempCol);
-          changed = true;
-          break;
-        }
-      }
-
-      if (!changed) {
-        col.push(value);
-      }
-    }
-
-    if (col.length > 5) {
-      col.length = 5;
-    }
+    return valueB - valueA;
   }
 
-  requestList.forEach(request => {
-    for (let j = 0; j < searchList.length; j++) {
-      let currentDocument = searchList[j];
-      let relevant = 0;
 
-      for (let i = 0; i < request.length; i++) {
-        let requestWord = request[i];
+  const searchList = parseString(documentsCollection);
+  const searchMap = {};
 
-        if (currentDocument[requestWord]) {
-          relevant += 1;
-        }
+  searchList.forEach((item, i) => {
+    Object.keys(item).forEach(searchWord => {
+      if (!searchMap.hasOwnProperty(searchWord)) {
+        searchMap[searchWord] = [i];
+      } else {
+        searchMap[searchWord].push(i);
       }
-      if (relevant > 0)
-        addFive(results, [relevant, j]);
-
-      relevant = 0;
-    }
-
-    answer.push(results);
-    results = [];
+    })
   });
 
-  return answer.map(item => item.map(element => element[1] + 1).join(' ')).join('\n');
+
+  const requestList = requests.map(item => item.split(' ').reduce((prev, cur) => {
+    prev[cur] = null;
+    return prev;
+  }, {}));
+  let answer = new Array(parseInt(requestsCount, 10));
+
+  requestList.forEach((request, requestIndex) => {
+    const requestCollection = Object.keys(request);
+
+    for (let i = 0; i < requestCollection.length; i++) {
+      let requestWord = requestCollection[i];
+
+      if (searchMap[requestWord]) {
+        const indexes = searchMap[requestWord];
+
+        indexes.forEach(index => {
+          if (answer[requestIndex]) {
+            if (answer[requestIndex].hasOwnProperty(index)) {
+              answer[requestIndex][index] += searchList[index][requestWord];
+            } else {
+              answer[requestIndex][index] = searchList[index][requestWord];
+            }
+
+          } else {
+            const arr = {};
+            arr[index] = searchList[index][requestWord];
+            answer[requestIndex] = arr;
+          }
+        });
+      }
+    }
+  });
+
+  return answer
+    .map((item) => {
+      return Object.keys(item)
+                .map(key => [item[key], parseInt(key)])
+                .sort(comparator)
+                .slice(0, 5);
+    })
+    .map(item => item.map(element => element[1] + 1)
+    .join(' '))
+    .join('\n')
 }
 
 var readline = require('readline');
@@ -104,14 +107,12 @@ io_interface.on('close', function () {
   console.log(solution(output_numbers));
 })
 
-/*console.log(solution(('7\n' +
-  'buy flat in moscow\n' +
-  'rent flat in moscow\n' +
-  'sell flat in moscow\n' +
-  'want flat in moscow like crazy in\n' +
-  'clean flat in moscow on weekends crazy\n' +
-  'renovate flat in moscow\n' +
-  'weekends in moscow for flat in\n' +
+/*console.log(solution(('6\n' +
+  'vfjabhs hmifg cxuzliah nnv vydtpq oyu rke eneo dbbnc occteh ovixjjuceb akjmcpoee ms nueamfz\n' +
+  'urklqk xiryl mxdsyh fmwvp ka ne lpw sdxcgkr xgywaxx eqgqmfcj rtgmv akdid tgzusr ecoyq fmwvp\n' +
+  'ggmj jdug ikpbjmj wgdkb zqm hshgpodp cw bmorjwlhwj fynprybuw zvwnpw fxfq lls sakzfec lap vusyr\n' +
+  'ezpsnpmzo ycvpow remdxqgv obnhspuk wubb vv ht mkpvpat fmwvp eu xlufvgxwt su muoscjnkb dnssnuqa\n' +
+  'lpw gfozlowmv lunvvv evs vyzpx zh juwzivkv cnyxrjym wqpaudztd uleeer myksx yyqdrxniyc oyu kcjjgcckq\n' +
+  'msogvjpuep ogk bmorjwlhwj pgmewgazl ir lap vpomyz npgjyaor gwoa bkwsj bglixoivq sw krgzivey\n' +
   '1\n' +
-  'flat in moscow for crazy weekends\n').split('\n')));
-*/
+  'rvwpiy fr vpomyz eneo zh wgdkb gwoa dt fmwvp rscbnnlz qdeu bmorjwlhwj yq eu myksx go yyqdrxniyc').split('\n')));*/
